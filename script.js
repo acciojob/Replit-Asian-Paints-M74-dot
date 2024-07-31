d98e66d61e56539acc995e929d692bf991992af9
Manisha Mali <82174253+M74-dot@users.noreply.github.com>
2024-07-31 15:41:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 //your JS code here. If required.
+document.addEventListener('DOMContentLoaded',()=>{
+	const changeBtn = document.querySelector('#change_button');
+	const resetBtn = document.querySelector('#reset');
+
+	changeBtn.addEventListener('click',()=>{
+		const blockId = document.querySelector('#block_id').value;
+		const colorId = document.querySelector('#colour_id').value;
+		const gridItems = document.querySelectorAll('.grid-item');
+		gridItems.forEach(item=>{
+			item.style.backgroundColor = 'transparent'
+		})
+		if(blockId && colorId){
+			const block = document.getElementById(blockId);
+			if (block) {
+                block.style.backgroundColor = colorId;
+            } else {
+                alert('Invalid block id');
+            }
+		} else {
+            alert('Please enter both block id and color');
+        }
+	});
+
+	resetBtn.addEventListener('click',()=>{
+		const gridItems = document.querySelectorAll('.grid-item');
+        gridItems.forEach(item => {
+            item.style.backgroundColor = 'transparent';
+        });
+	});
+});
